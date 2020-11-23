05160cdefaf72b1a1c7797b90a92761af793e0e8
dickson507 <ngsonjim507@gmail.com>
2020-11-23 21:57:12 +0800

    add test folder

---
+++ src/SystemTest/Test_Main.java
@@ -0,0 +1,19 @@
+package SystemTest;
+
+import java.io.*;
+import java.util.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import MainSystem.*;
+
+public class Test_Main {
+
+		
+	@Test
+    // Condition Coverage - Result False #1
+    public void test_01() throws Exception {
+        
+    }
+
+
+}
