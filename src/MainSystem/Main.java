61a20785be3d6ff206877c1890ae1f66d56da535
dickson507 <ngsonjim507@gmail.com>
2020-11-26 13:10:30 +0800

    modify main in alan branch

--- src/MainSystem/Main.java
+++ src/MainSystem/Main.java
@@ -16,7 +16,23 @@ import Kitchen.*;
 
 
 public class Main {
+	
+	public static void getInputFromFile(Reception reception, String filepath) throws FileNotFoundException {
+		Scanner scanner = new Scanner(new File(filepath));
+		while (scanner.hasNextLine()) {
+			reception.input_order(scanner.nextLine());
+			System.out.println("Order appended");
+		}
+		scanner.close();
+	}
 
+	public static void kitchenProcess(Reception reception, Kitchen kitchen) {
+		ArrayList<CustomerOrder> co = reception.getCustomerOrderQueue();
+		kitchen.process(co);
+		System.out.println("Process completed");
+	}
+	
+	
 	public static void main(String args[]) throws IOException {
 		// get the reception of the restaurant!
 		Reception reception = Reception.getReception();
@@ -29,19 +45,11 @@ public class Main {
 				insertDish(dish);
 			}
 			
+			getInputFromFile(reception, "./src/MainSystem/myfile.txt");	
 			
-			Scanner scanner = new Scanner(new File("./src/MainSystem/myfile.txt"));
-			while (scanner.hasNextLine()) {
-				reception.input_order(scanner.nextLine());
-				System.out.println("Order appended");
-			}
-			scanner.close();
-			
-			reception.getListOfOrders();
-			
+//			reception.getListOfOrders();
 			Kitchen kitchen = new Kitchen("Abby", "001", "Bobby", "002", "Chabby", "003");
-			ArrayList<CustomerOrder> co = reception.getCustomerOrderQueue();
-			kitchen.process(co);
+			kitchenProcess(reception, kitchen);
 			
 		}
 		catch (FileNotFoundException e) {
